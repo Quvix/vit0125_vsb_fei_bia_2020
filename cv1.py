171b861c91e33159fc4ee40e822ee48e36f04275
quvix <quvixcz@gmail.com>
2020-10-04 22:55:45 +0200

    removing unused code

--- cv1.py
+++ cv1.py
@@ -1,13 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import FuncAnimation
-import math
 from functions import *
 from solution import Solution
 from algorithms import *
-
-#print(np.random.normal(0, 0.1, 100))
 
 s = Solution(2, -5.12, 5.12, 0.1, sphere, blind_search)
 print(s.find_minimum(100, 10))
